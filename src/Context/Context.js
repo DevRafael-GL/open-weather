54f35125b71157b011e44a80cb5801c6cf1866aa
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2022-01-21 07:37:08 -0300

    background

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -5,8 +5,11 @@ export const GlobalContext = createContext();
 const Context = ({ children }) => {
   const [local, setLocal] = React.useState("Belo Horizonte");
   const [erro, setErro] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   return (
-    <GlobalContext.Provider value={{ local, setLocal, erro, setErro }}>
+    <GlobalContext.Provider
+      value={{ local, setLocal, erro, setErro, loading, setLoading }}
+    >
       {children}
     </GlobalContext.Provider>
   );
